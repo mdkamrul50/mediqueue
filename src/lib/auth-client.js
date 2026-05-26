2d93b0c29f8578ec29cdaf71915e8d6faff47f4c
mdkamrul50 <kamrul0088e@gmail.com>
2026-05-26 20:56:47 +0800

    feat: update authClient baseURL to production URL

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://mediqueue-two.vercel.app',
 });
 
 export const { signIn, signUp, useSession } = authClient;
